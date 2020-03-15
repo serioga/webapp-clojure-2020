d72b1725a842a3c86e94456e77e05d831b046432
serioga <github@sergey.trofimov.name>
2020-03-15 17:43:08 +0100

    Review mdc context implementation // Add wrap-with-keys

--- src/app/lib/http_handler/middleware/logging_context.clj
+++ src/app/lib/http_handler/middleware/logging_context.clj
@@ -1,7 +1,6 @@
 (ns app.lib.http-handler.middleware.logging-context
   (:require
-    [app.lib.util.mdc :as mdc]
-    [app.lib.util.perf :as p])
+    [app.lib.util.mdc :as mdc])
   (:import
     (java.util UUID)))
 
@@ -12,7 +11,6 @@
   "Wrap handler with MDC logging context."
   [handler]
   (fn [request]
-    (mdc/wrap-with-map (-> request
-                           (p/fast-select-keys [:server-name :route-tag :session])
-                           (p/fast-assoc :request-id (UUID/randomUUID)))
-      (handler request))))
+    (mdc/wrap-with-keys request [:server-name :route-tag :session]
+      (mdc/wrap-with-map {:request-id (UUID/randomUUID)}
+        (handler request)))))
--- src/app/lib/util/mdc.clj
+++ src/app/lib/util/mdc.clj
@@ -8,19 +8,36 @@
 
 
 (defn add-context-map
-  "Add keys/values from Clojure/Java map to logging context (as side-effect)."
-  [m]
-  (cond
-    ; Process clojure map with conversion of keys/values to strings.
-    (map? m)
-    (reduce-kv (fn [_ k v] (MDC/put (name k) (str v))), nil, m)
+  "Add keys/values from Clojure/Java map to logging context (as side-effect).
+   When `ks` sequence provided then only these keys are taken."
+  ([m]
+   (cond
+     ; Process clojure map with conversion of keys/values to strings.
+     (map? m)
+     (reduce-kv (fn [_ k v] (MDC/put (name k) (str v))), nil, m)
 
-    ; Process java map without conversion of keys/values.
-    (instance? Map m)
-    (reduce (fn [_ k] (MDC/put k (.get ^Map m k))), nil, (.keySet ^Map m))
+     ; Process java map without conversion of keys/values.
+     (instance? Map m)
+     (reduce (fn [_ k] (MDC/put k (.get ^Map m k))), nil, (.keySet ^Map m))
 
-    ; Ignore non-maps.
-    :else nil))
+     ; Ignore non-maps.
+     :else nil))
+  ([m ks]
+   (cond
+     ; Process clojure map with conversion of keys/values to strings.
+     (map? m)
+     (reduce (fn [_ k] (when-some [v (m k)]
+                         (MDC/put (name k) (str v))))
+             nil, ks)
+
+     ; Process java map without conversion of keys/values.
+     (instance? Map m)
+     (reduce (fn [_ k] (when-some [v (.get ^Map m k)]
+                         (MDC/put k v)))
+             nil, ks)
+
+     ; Ignore non-maps.
+     :else nil)))
 
 
 (defn get-context-map
@@ -32,10 +49,10 @@
 
 
 (defmacro wrap-with-map
-  "Execute `body` wrapped with added context map.
+  "Execute `body` wrapped with added context from map.
    Restore context map at the end."
-  [context-map & body]
-  `(let [context-map# ~context-map
+  [m & body]
+  `(let [context-map# ~m
          old-context# (MDC/getCopyOfContextMap)]
      (try
        (add-context-map context-map#)
@@ -43,3 +60,18 @@
        (finally
          (if old-context# (MDC/setContextMap old-context#)
                           (MDC/clear))))))
+
+
+(defmacro wrap-with-keys
+  "Execute `body` wrapped with added context for keys from map.
+   Restore context map at the end."
+  [m ks & body]
+  `(let [context-map# ~m
+         old-context# (MDC/getCopyOfContextMap)]
+     (try
+       (add-context-map context-map# ~ks)
+       ~@body
+       (finally
+         (if old-context# (MDC/setContextMap old-context#)
+                          (MDC/clear))))))
+
